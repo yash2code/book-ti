ac100e8d4dc3759f9d452a50dfa4ceef40b3719b
Yash  Chaudhary <yash.chaudhary@Kuliza-505.local>
2019-08-17 21:41:39 +0530

    Updates

---
+++ precache-manifest.ec44f292ab17d956661f2982684dcba0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd3b32ec62d9d586e18e261f2037d969",
+    "url": "/bookmyshow-houm/index.html"
+  },
+  {
+    "revision": "1a2b10ca04c9c8a3c751",
+    "url": "/bookmyshow-houm/static/css/main.a881e47a.chunk.css"
+  },
+  {
+    "revision": "d5f256cb175941192cce",
+    "url": "/bookmyshow-houm/static/js/2.5197114e.chunk.js"
+  },
+  {
+    "revision": "1a2b10ca04c9c8a3c751",
+    "url": "/bookmyshow-houm/static/js/main.f5fbe468.chunk.js"
+  },
+  {
+    "revision": "77483039af123a99de15",
+    "url": "/bookmyshow-houm/static/js/runtime~main.90207599.js"
+  }
+]);
